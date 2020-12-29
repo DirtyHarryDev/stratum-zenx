4d37caa54396f00cb471308d1c5056af59a22947
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2020-12-29 04:16:21 +0100

    Update balloon.h

--- algos/balloon.h
+++ algos/balloon.h
@@ -17,9 +17,14 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifndef BALLOON_H
+#define BALLOON_H
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include <stdint.h>
 
 int alx_init_balloon_buffer();
 void balloon(const char* input, char* output, unsigned int len);
@@ -30,3 +35,4 @@ void alx_free_balloon_buffer();
 }
 #endif
 
+#endif
